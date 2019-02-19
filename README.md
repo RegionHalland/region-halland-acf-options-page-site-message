5b1ccc3026491d3fb4f534f078320d71a1d6636b
rhn014 <Roland.Hyden@RegionHalland.se>
2019-02-19 16:40:21 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# region-halland-acf-options-page-site-message
-Skapar en sida i admin med ett formulär för "Viktiga meddelanden"
+# Skapa en sida i admin med ett formulär för 'Viktiga meddelanden'
+
+## Hur man använder Region Hallands plugin "region-halland-options-page-site-message"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-options-page-site-message".
+
+
+## Användningsområde
+
+Denna plugin skapar en array() med all formulärdata för viktiga meddelanden
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-options-page-site-message.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-options-page-site-message.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-options-page-site-message": "1.0.0"
+},
+```
+
+
+## Visa "Viktigt meddelande" via "Blade"
+
+```sh
+@php($myMessage = get_region_halland_acf_site_message())    
+@if($myMessage['show_message'] == true)
+  <span>{!! $myMessage['rubrik'] !!}</span><br>
+  <span>{!! $myMessage['meddelande'] !!}</span><br>
+  <a href="{{ $myMessage['link_url'] }}" target="{{ $myMessage['link_target'] }}">{{ $myMessage['link_title'] }}</a>
+@endif
+```
+        
+
+## Exempel på hur arrayen kan se ut
+
+```sh
+array (size=6)
+  'rubrik' => string 'Viktigt meddelande' (length=18)
+  'meddelande' => string 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' (length=56)
+  'link_title' => string 'Lorem Ipsim' (length=11)
+  'link_url' => string 'http://exempel.se/lorem-ipsum/' (length=30)
+  'link_target' => string '_blank' (length=6)
+  'show_message' => boolean true
+```
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_acf_options_page_site_message.php
@@ -0,0 +1,216 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Options Page Site Message
+	 */
+	/*
+	Plugin Name: Region Halland ACF Options Page Site Message
+	Description: Skapa en subsida till theme.php för "viktigt meddelande"
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: MIT
+	Text Domain: regionhalland
+	*/
+
+	// Anropa funktion om ACF är installerad & aktiverad
+	add_action('acf/init', 'region_halland_acf_add_options_page_site_message');
+
+	// Lägg till en sida för viktigt meddelande
+	function region_halland_acf_add_options_page_site_message() {
+
+		if( function_exists('acf_add_options_page') ) {
+		
+			$option_page = acf_add_options_page(array(
+				'page_title' 	=> __('Viktigt meddelande', 'regionhalland'),
+				'menu_title' 	=> __('Viktigt meddelande', 'regionhalland'),
+				'menu_slug' 	=> 'theme-general-settings-viktigt-meddelande'
+			));
+			
+		}
+	
+	}
+
+	// Anropa funktion om ACF är installerad & aktiverad
+	add_action('acf/init', 'my_acf_add_site_message_field_groups');
+
+	// Lägg till ett formulär
+	function my_acf_add_site_message_field_groups() {
+
+		// Om funktionen existerar
+		if (function_exists('acf_add_local_field_group')):
+
+			// Lägg till formulär
+			acf_add_local_field_group(array(
+				'key' => 'group_1000070',
+				'title' => 'Hantera text för viktigt meddelande',
+				'fields' => array(
+					array(
+						'key' => 'field_1000071',
+						'label' => 'Meddelande',
+						'name' => 'name_1000072',
+						'type' => 'tab',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'placement' => 'top',
+						'endpoint' => 0,
+					),
+					array(
+						'key' => 'field_1000073',
+						'label' => '',
+						'name' => 'name_1000074',
+						'type' => 'group',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'layout' => 'block',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_1000075',
+								'label' => 'Rubrik',
+								'name' => 'name_1000076',
+								'type' => 'text',
+								'instructions' => 'Skriv rubrik (max antal tecken = 50)',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'maxlength' => '50',
+								'rows' => 3,
+								'new_lines' => '',
+							),
+							array(
+								'key' => 'field_1000077',
+								'label' => 'Meddelande',
+								'name' => 'name_1000078',
+								'type' => 'textarea',
+								'instructions' => 'Skriv meddelande (max antal tecken = 150)',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'placeholder' => '',
+								'maxlength' => '150',
+								'rows' => 3,
+								'new_lines' => '',
+							),
+							array(
+				              'key' => 'field_1000079',
+		            		  'label' => __('Länk', 'regionhalland'),
+		            		  'name' => 'name_1000080',
+		            		  'type' => 'link',
+		            		  'instructions' => __('Länk till valfri sida. Kan vara en extern länk, en sida eller ett inlägg.', 'regionhalland'),
+		            		  'required' => 0,
+		            		  'conditional_logic' => 0,
+		            		  'wrapper' => array(
+		                		'width' => '',
+		                		'class' => '',
+		                		'id' => '',
+		            		  ),
+		            		  'return_format' => 'array',
+			        		  ),
+							  array(
+					          	'key' => 'field_1000081',
+					            'label' => __('Välj om detta meddelande ska synas eller inte', 'regionhalland'),
+					            'name' => 'name_1000082',
+					            'type' => 'checkbox',
+					            'instructions' => '',
+					            'required' => 0,
+					            'conditional_logic' => 0,
+					            'wrapper' => array(
+					                'width' => '',
+					                'class' => '',
+					                'id' => '',
+					            ),
+					            'choices' => array(
+					                'choice_1000083' => __('Visa meddelande', 'regionhalland'),
+					            ),
+					            'allow_custom' => 0,
+					            'save_custom' => 0,
+					            'default_value' => array(
+					            ),
+					            'layout' => 'vertical',
+					            'toggle' => 0,
+					            'return_format' => 'value',
+					    	),
+						),
+					)
+				),
+				'location' => array(
+					array(
+						array(
+							'param' => 'options_page',
+							'operator' => '==',
+							'value' => 'theme-general-settings-viktigt-meddelande',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => 1,
+				'description' => '',
+			));
+
+		endif;
+
+	}
+
+	function get_region_halland_acf_site_message() {
+
+		// Define array
+		$myMessage = array();
+		
+		// Fill array with data
+		$myMessage['rubrik'] 		= get_field('name_1000074_name_1000076', 'options');
+		$myMessage['meddelande'] 	= get_field('name_1000074_name_1000078', 'options');
+		$arrLink 					= get_field('name_1000074_name_1000080', 'options');
+		$myMessage['link_title'] 	= $arrLink['title'];
+		$myMessage['link_url'] 		= $arrLink['url'];
+		$myMessage['link_target'] 	= $arrLink['target'];
+		$myMessage['show_message'] 	= is_array(get_field('name_1000074_name_1000082', 'options'));
+
+		// Return array
+		return $myMessage;
+
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_options_page_site_message_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen av-aktiveras
+	function region_halland_acf_options_page_site_message_deactivate() {
+		// Ingenting just nu...
+	}
+
+	// Aktivera pluginen och anropa metod
+	register_activation_hook( __FILE__, 'region_halland_acf_options_page_site_message_activate');
+
+	// Av-aktivera pluginen och anropa metod
+	register_deactivation_hook( __FILE__, 'region_halland_acf_options_page_site_message_deactivate');
+
+?>
